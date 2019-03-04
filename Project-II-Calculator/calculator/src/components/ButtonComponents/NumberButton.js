e9e6fa7b4dfdbc0755bc17109a233863c133e2d5
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-03 19:02:30 -0500

    MVP Complete

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton (props) {
+    let newClass = ''
+    if (props.digits === 'clear' || props.digits === 0) {
+        newClass = 'clear';
+    } else {
+        newClass = 'numbers'
+    }
+
+    return (
+        <button className = {newClass}>{props.digits}</button>
+    )
+}
+
+export default NumberButton
